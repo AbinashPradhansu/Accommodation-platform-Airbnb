ebfcadfed600903d7856fb069e76bb4193f19550
Vamsi0005 <vamsisalla61@gmail.com>
2025-11-27 12:17:32 +0530

    Fix Cloudinary allowedFormats bug

--- cloudConfig.js
+++ cloudConfig.js
@@ -2,21 +2,57 @@ const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
-    cloud_name:process.env.CLOUD_NAME,
-    api_key:process.env.CLOUD_API_KEY,
-    api_secret:process.env.CLOUD_API_SECRET,
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET,
 });
-
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: 'wonderlust_DEV',
-   allowerFormats: ["png","jpg","jpeg"],
+    allowedFormats: ["png", "jpg", "jpeg"],
  },
 });
 
-module.exports={
+module.exports = {
     cloudinary,
     storage,
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const cloudinary = require('cloudinary').v2;
+// const { CloudinaryStorage } = require('multer-storage-cloudinary');
+
+// cloudinary.config({
+//     cloud_name:process.env.CLOUD_NAME,
+//     api_key:process.env.CLOUD_API_KEY,
+//     api_secret:process.env.CLOUD_API_SECRET,
+// });
+
+
+// const storage = new CloudinaryStorage({
+//   cloudinary: cloudinary,
+//   params: {
+//     folder: 'wonderlust_DEV',
+//    allowerFormats: ["png","jpg","jpeg"],
+//  },
+// });
+
+// module.exports={
+//     cloudinary,
+//     storage,
+// };
